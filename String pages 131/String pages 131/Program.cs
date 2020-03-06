9c34b4c5934c7d640c84c9ee1446d792f8dae258
samuel <samuelsyameogo@gmail.com>
2020-03-06 14:09:46 -0800

    c

---
+++ String pages 131/String pages 131/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace String_pages_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string firstName = "Samuel";
+            Console.WriteLine(firstName);
+
+            String lastName = "Yameogo";
+            Console.WriteLine(lastName);
+
+            string midName = "Sam";
+            Console.WriteLine(midName);
+
+            string name = "Samuel Sam  Yameogo";
+            name = name.ToUpper();
+            Console.WriteLine(name);
+
+            String quote = "Madames, Moidemoiselles, et Messiers. \"Bonjour\",\nBonjour Tonton nouvelle ligne.\n \t Bonjour mes amies.";
+            Console.WriteLine(quote);
+
+
+            StringBuilder sb = StringBuilder();
+            sb.Append("Je m'appel Samuel");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
