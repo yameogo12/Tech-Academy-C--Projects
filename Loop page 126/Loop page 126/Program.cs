30d37771515be52a9b8c0537c637a3e314d3cdfe
samuel <samuelsyameogo@gmail.com>
2020-03-06 14:00:55 -0800

    c

---
+++ Loop page 126/Loop page 126/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loop_page_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello This Is a LOOP!");
+
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n < 5);
+            Console.ReadLine();
+
+        }
+    }
+}
