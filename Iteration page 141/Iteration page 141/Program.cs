98839eacebf3ac2e20971ac19e9f3734b7f6705e
samuel <samuelsyameogo@gmail.com>
2020-03-09 16:46:42 -0700

    Update Program.cs

--- Iteration page 141/Iteration page 141/Program.cs
+++ Iteration page 141/Iteration page 141/Program.cs
@@ -7,34 +7,51 @@ namespace Iteration_page_141
     {
         static void Main(string[] args)
         {
-            //string[] houses = { "townhouse", "treehouse", "appartment" };
-            //Console.WriteLine("put some text here");
-            //string sentence1 = Console.ReadLine();
-            //Console.WriteLine("your  firt sentence is ," + sentence1);
 
-            //  for ( int x = 0; x <houses.Length; x--)
-            // {
-            //  Console.WriteLine("houses:{0}" , x);
-            // }
-            //Console.ReadLine();
 
-            //for (int x = 0; x <houses.Length; x++)
+            string[] houses = { "townhouse", "treehouse", "appartment" };
+            Console.WriteLine(" Choose a cool name");
+            string Name = Console.ReadLine();
+            
+            
+           // string sentence1 = Console.ReadLine();
+           // Console.WriteLine("your  firt sentence is ," + sentence1);
+
+            //for (int x = 0; x < houses.Length; x--)
             //{
-            //    Console.WriteLine(houses[x]);
+            //    Console.WriteLine("houses:{0}", x);
             //}
             //Console.ReadLine();
 
+           foreach(string house in houses)
+              {
+                Console.WriteLine(Name + house);
+                //if (Name != house)
+                //{
+                //    Console.WriteLine("That name is not on this array");
+                }
+                Console.ReadLine();
+            }
+
+            //foreach (int x = 0; x < houses.Length; x++)
+            //{
+              //  if (houses[x] < 3)  
+               //{
+                 //   Console.WriteLine(" " + houses[x]);
+                //}
+            //}
+            //Console.ReadLine();
 
             //List<string> names = new List<string>() { "townhouse", "treehouse", "appartment" };
 
             //foreach (string name in names)
             //{
-                
-            //    {
-            //        Console.WriteLine(name);
-            //    }
-            //}
-            //Console.ReadLine();
+
+            //  {
+            //   Console.WriteLine(name + names);
+            // }
+            // }
+            // Console.ReadLine();
 
 
             //List<string> names = new List<string>() { "townhouse", "treehouse", "appartment" };
@@ -48,18 +65,22 @@ namespace Iteration_page_141
             //}
             //Console.ReadLine();
 
-            List<string> names = new List<string>() { "townhouse","townhome","treehouse","treehouse","appartment"};
-            List<string> coolNames = new List<string>();
-            foreach (string real in names)
-            {
-                if(real == "treehouse")
-                {
-                    coolNames.Add("names");
-                }
-            }
-            Console.WriteLine(coolNames.Count);
-            Console.ReadLine();
+            //List<string> names = new List<string>() { "townhouse","townhome","treehouse","treehouse","appartment"};
+            //List<string> coolNames = new List<string>();
+            //foreach (string real in names)
+            //{
+            //    if(real == "")
+            //    {
+            //        coolNames.Add("names");
+            //   }
+            //}
+
+
+
+
+            //Console.WriteLine(coolNames.Count);
+            //Console.ReadLine();
 
         }
     }
-}
+
