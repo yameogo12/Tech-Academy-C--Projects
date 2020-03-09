82455b32299f9cf3115dcd0fff776f5e3f7d43e4
samuel <samuelsyameogo@gmail.com>
2020-03-09 09:17:14 -0700

    add
    c

---
+++ ITERATION Lessons/ITERATION Lessons/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITERATION_Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    //Console.WriteLine("Hello World!");
+            //    //Console.ReadLine();
+
+            //    int[] testScores = { 98, 99, 95, 85, 70, 82, 34, 91, 90, 94 };
+
+            //    for (int i = 0; i < testScores.Length; i ++ )
+            //    {
+            //        if (testScores[i] > 85)
+            //        {
+            //            Console.WriteLine("Passing test score: " + testScores[i]);
+            //         }
+            //     }
+
+            //Console.ReadLine();
+
+        //    string[] names = { "sam", "yam", "sidbe" };
+
+        //    //   for (int y = 0;  y < names.Length; y++)
+        //    //   {
+        //    //       if (names[y] =="yam")
+        //    //   }
+        //    //   Console.WriteLine(names[y]);
+        //    //}
+        //    //   Console.ReadLine();
+
+        //    for (int y = 0; y < names.Length; y++)
+        //    {
+        //        Console.WriteLine(names[y]);
+        //    }
+        //    Console.WriteLine();
+
+
+
+        }
+
+
+
+    }
+
+}
+
+
+
+    
+    
